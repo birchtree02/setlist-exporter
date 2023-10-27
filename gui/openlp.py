83952ba96cbc87adee5eae4d3b89cbccbe1fb13f
Edward Birchall <edward@birchall.org.uk>
2023-10-27 22:57:56 +0100

    Implement OpenLP exporter
    Still TODO:
    - warn the user if the song is probably different
    - option to download an XML version of the SBP file
    - option to refresh the OpenLP database
        - possible to do this on return after lost focus?

--- gui/openlp.py
+++ gui/openlp.py
@@ -1,25 +1,39 @@
 import os
+import re
 
 from nicegui import ui
 from state import ApplicationState
+import song_tools.openlp
+from song_tools.song import Song
+from song_tools.openlp import service
+from song_tools import openlyrics_parser
 
 
 def main(state: ApplicationState):
-    def handle_openlp_path(e):
-        path = path_from_user.value
+    # TODO: move setup stuff to a different function
+    def handle_openlp_path(_, path):
         if path.endswith("/") or path.endswith("\\"):
             path = path[:-1]
         if not path.endswith("openlp"):
             ui.notify("Path must end with 'openlp'")
         else:
             # TODO: check this is actually a valid openlp data folder
+            openlp_file = os.path.join(state.dir, "openlp.txt")
             with open(openlp_file, "w") as f:
                 f.write(path)
             ui.notify("Path saved")
             div.clear()
             main(state)
 
-    with ui.element("div") as div:
+    def handle_openlp_opened(_):
+        if service.available():
+            ui.notify("Connected to OpenLP!")
+            div.clear()
+            main(state)
+        else:
+            ui.notify("OpenLP not found. Try again!")
+
+    def setup():
         # Get OpenLP data folder from openlp.txt
         # check openlp.txt exists
         openlp_file = os.path.join(state.dir, "openlp.txt")
@@ -28,15 +42,101 @@ def main(state: ApplicationState):
             # If it doesn't exist, ask user for
             ui.label("No data folder found. Enter path below:")
             path_from_user = ui.input("openlp_path").on(
-                "keydown.enter", handle_openlp_path
+                    "keydown.enter", lambda e: handle_openlp_path(e, path_from_user)
             )
+        elif not service.available():
+            ui.label("OpenLP doesn't appear to be open. Open it, then click below")
+            ui.button("I've opened OpenLP!", on_click= lambda e: handle_openlp_opened(e, service))
         else:
             # If it does exist, read it
             with open(openlp_file, "r") as f:
                 openlp_path = f.read()
 
             # Then, load it
+            data_folder = openlp_path+"/data"
+            state.openlp_db = song_tools.openlp.load_database(data_folder)
 
-            # Then, show the list of songs
+            return True
+
+    def render_song(song: Song):
+        # TODO: warn the user if the OpenLP song is probably incorrect (e.g. if over 50% of lines are different)
+        # TODO: option to download xml song to be imported to OpenLP
+        # TODO: some way of refreshing the OpenLP database
+        def show_diff(song, openlp_song):
+            with ui.dialog(value=True) as dialog, ui.card():
+                # TODO: cange name of first_match and first_match_song
+                first_match = (openlp_song[0], openlp_song[6], openlp_song[2], openlp_song[7])
+                first_match_song = openlyrics_parser.OpenLyricsParser().parse_open_lyrics(
+                    *first_match[:3]
+                )
+
+                diff_string1, diff_string2 = song.diff(first_match_song)
+                with ui.element("div").classes("grid grid-cols-2"):
+                    # TODO: compress this code by having colour_by_start_character take a list of characters and their colours
+                    with ui.element("div"):
+                        ui.markdown("**SBP showing lines not in OpenLP**")
+                        if diff_string1:
+                            diff_string1 = colour_by_start_character(
+                                diff_string1, char="\-", colour="red"
+                            )
+                            diff_string1 = colour_by_start_character(
+                                diff_string1, char="\%", colour="orange"
+                            )
+                            diff_string1 = colour_by_start_character(
+                                diff_string1, char="\*", colour="orange"
+                            )
+                            ui.markdown(diff_string1.replace("\n", "<br>"))
+                        else:
+                            ui.markdown("*All lines in SBP are in OpenLP*")
+                    with ui.element("div"):
+                        ui.markdown("**OpenLP showing lines not in SBP**")
+                        if diff_string2:
+                            diff_string2 = colour_by_start_character(
+                                diff_string2, char="\+", colour="green"
+                            )
+                            diff_string2 = colour_by_start_character(
+                                diff_string2, char="\%", colour="orange"
+                            )
+                            diff_string2 = colour_by_start_character(
+                                diff_string2, char="\*", colour="orange"
+                            )
+                            ui.markdown(diff_string2.replace("\n", "<br>"))
+                        else:
+                            ui.markdown("*All lines in OpenLP are in SBP*")
+
+                ui.button(
+                    "Add to OpenLP",
+                    on_click=lambda: service.add_song_by_id(first_match[3]),
+                )
+
+
+
+
+        name, openlp_result = song_tools.openlp.find_song(song, state.openlp_db)
+        ui.markdown(f"Matched with __{name.title()}__ in OpenLP.")
+        ui.button("View differences", on_click=lambda: show_diff(song, openlp_result))
+        ui.button("Add to set", on_click=lambda: service.add_song_by_id(openlp_result[7]))
+
+
+    with ui.element("div") as div:
+        if not setup():
+            return
+
+        with ui.stepper().props("vertical").classes("w-full bg-slate-300") as stepper:
+            # TODO: Move the stepper into a shared function with spotify/other modules which takes a rendering function as parameter
             for song in state.song_list:
-                ui.label(song.title)
+                with ui.step(song.title):
+                    render_song(song)
+                    with ui.stepper_navigation():
+                        ui.button("Next", on_click=stepper.next)
+                        ui.button("Back", on_click=stepper.previous).props("flat")
+
+
+def colour_by_start_character(diff_string, char, colour):
+    return re.sub(
+        r"^(" + char + ".*)$",
+        rf'<span style="color: {colour};">\1</span>',
+        diff_string,
+        flags=re.MULTILINE,
+    )
+
--- state.py
+++ state.py
@@ -1,8 +1,12 @@
+from typing import List
+
+from song_tools.song import Song
+
 class ApplicationState:
     def __init__(self):
-        self.sbp_imported = False
-        self.song_list: List[Song] = None
-        self.openlp_db = None
-        self.openlp_connected = False
-        self.spotify_connected = False
-        self.dir = ""
+        self.sbp_imported: bool = False
+        self.song_list: List[Song] = []
+        self.openlp_db: List[tuple] = []
+        self.openlp_connected: bool = False
+        self.spotify_connected: bool = False
+        self.dir: str = ""
